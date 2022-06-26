925f87e11357949b26aeaff229a69ea391e40264
cebollatibia <105892985+cebollatibia@users.noreply.github.com>
2022-06-25 23:13:26 -0500

    js

---
+++ js/botones.js
@@ -0,0 +1,41 @@
+function f (){
+    let mision1 = document.getElementById("mision1")
+    mision1.innerHTML = "Corre con el capitan (solo te aran poco daño)al recoger a cortana devez acerte espacio entre los marins despues sigue las instrucciones de cortana cuando lleges a el area de las granadas lanzalas nota:cambia constantemente de armas la mision es corta."
+}
+function z (){
+    let mision1 = document.getElementById("mision1")
+    mision1.innerHTML = "Muy bien aqui lo que tienes que hacer es correr al otro lado del puente posterirmente correr a las colinas y enfrentar al pacto en el acto enfrentalos a la distancia si se te dificulta rushear hazlo a la distancia te recomiendo el lanzador de municiones giadas tipo 33 al llegar martillo debes ir a la cueva artificial puedes evitar algunos enemigos o atropellarlos cuando vas a juntar a los aliados ve por el camino predetrminado posrque yo como el pendejo que soy me fui a lo grande y mori numerosas veses en el intento nota si consigues el franco tirador derriba a la mayor cantidad de enemigos"
+}
+
+function w (){
+    let mision1 = document.getElementById("mision1")
+    mision1.innerHTML = "<b>No te expongas</b> en esta mision derriba a el pacto a la distancia y pocicionate en los cañones fijos exepto cuando los Mgalekgolo aparecen en ese momento bajate y lanzales granadas sigue las indicaciones de cortana, cuando te encuentras con el capitan tu ve por enfrnte ya que te querran matar a ti primero"
+}
+function s (){
+    let mision1 = document.getElementById("mision1")
+    mision1.innerHTML = "Cubrete y atropella!! cuando estes en el cartografo trata de evitarlos cuando cortana escanee los datos ¡corre! sal y cuando lleges afuera espera, esta mision es si es facil a ecepción del cartografo"
+}
+function a (){
+    let mision1 = document.getElementById("mision1")
+    mision1.innerHTML = "Se cauteloso y no llames la atencion si no salta, ataca, corre y repite al llegar a la sala te tendras que cubrir detras de una estructura asi podras evitar la muerte instantanea"
+}
+function d (){
+    let mision1 = document.getElementById("mision1")
+    mision1.innerHTML = "Al inicio parece facil como el inicio de el cartografo pero al rato de un tiempo aparecen los flood y son muchos pero con una escopeta se resuelve todo, algo mas no mires atras los que mas molestan son los chiquitos ya que son numerosos utiliza un rifle de asalto"
+}
+function e (){
+    let mision1 = document.getElementById("mision1")
+    mision1.innerHTML = "En esta mición solo aparece el flood y solo necesitaras una escopeta y el rifle de asalto al igual que en ataque a la sala corre, salta, ataca, lanza granada y repite"
+}
+function q (){
+    let mision1 = document.getElementById("mision1")
+    mision1.innerHTML = "Pelea con los z-1500 debes salir de la sala con cautela al salir debes escoger tus armas favoritas y es lo mismo que la mición 5 la misma logica incluido que tienes que derribar los veiculos trata de ignorar las batallas de los covenad y los flood solo avana al destruir los reactores aslo rapido y mantente alerta mata a todos para que te sientas mas seguro con la tecnica corre, salta, ataca y repite cuando recogas los bamshee mata solo las tropas aereas o veiculos y torres y manten una posición cuando destruyas los generadores cuando vallas por el tercer generador trata de ser cauteloso y ataca a la distancia te tomara tiempo pero sera mas facil en el tercer gemnerador toma una pocicion y mata a lo z-1500 y listo"
+}
+function r (){
+    let mision1 = document.getElementById("mision1")
+    mision1.innerHTML = "En este momento seras transportado a el verdad y reconciliacion por el capitan trata de evitar las preleas(vete por lo oscurito)deja que los covenad y los flod se pelen entre ellos al regresar a la nave pelea en la entrada cuando lleges con keyes, cuando tengas los chips de activacion alejate y deja que el flood se encarge luego los matas de preferencia con la escopeta M90 ojito cuando salgas te recomiendo un truco muy recomendado en el cual cuando caigas de una altura considerable lo hagas presinando shift esto te ayudara cuando tengas que escapar, antes de que llegen los covenad baja de la plataforma de arriba de la manera que te enseñe ahora solo subete t convate en lo que se activa la sinematica"
+}
+function c (){
+    let mision1 = document.getElementById("mision1")
+    mision1.innerHTML = "Al entrar ve rapido a los ductos de ventilacion del pilar of autum y cuando tengas la oportunidad de encontrar un flod con escopeta robasela trata de evitar a los z-1500 e ignora las vatallas entre flood y covenand al termino de la sinematica dirigite por el camino predeterminado trata de ignorar las batallas, si eres atento te daras cuenta de que en el piso ponen flechas que indican que ese camino lleva a tal lugar guiate con esas señales, cuando detrullas los generadores utiliza granadas u un anti veiculo de medio cohete posterirmente ve a los veiculos y subete en el primero que te encuentres trata de ir en el centro y lo mas rapido posible ignora por completo a los covenand y flood incluso antes de la ultima cinematica"
+}
